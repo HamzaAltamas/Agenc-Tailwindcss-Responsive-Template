606df9731563eafc0758fa88cd15b11e3f0ac844
Hamza Altamas <hamza.altamas.01876@gmail.com>
2022-09-14 20:45:51 +0600

    Full responsive navbar added

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        purple: "#6A4DF4",
+        lightpurple: "#F0EDFE",
+        ashtext: "#737373",
+      },
+      fontFamily: {
+        int: "Inter",
+      },
+      maxWidth: {
+        container: "1100px",
+      },
+      fontSize: {
+        btntxt: "20px",
+      },
+      borderRadius: {
+        btnradious: "10px",
+      },
+    },
+  },
+};
